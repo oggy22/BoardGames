9cfa6c7a9899ff7228fe23650d18e6d6a1e6bba5
Ognjen Sobajic <oggy22@gmail.com>
2024-12-08 16:34:28 -0800

    Add control_center for chess evaluate function.

--- BoardGamesEngine/Games/chess.h
+++ BoardGamesEngine/Games/chess.h
@@ -494,7 +494,7 @@ namespace chess {
                 for (int y = miny; y <= maxy; y++)
                 {
                     Piece piece = table[Square(x, y)];
-                    ret += sgn(piece);
+                    ret += int(sgn(piece));
                 }
             return ret;
         }
@@ -505,21 +505,32 @@ namespace chess {
                 + king_protection_eval(this->King2);
         }
 
+        int count_center_pieces()
+        {
+			return
+                int(sgn(table[S("D4")])) +
+                int(sgn(table[S("E4")])) +
+                int(sgn(table[S("D5")])) +
+                int(sgn(table[S("E5")]));
+        }
+
         template <
             int material = 1, //Qs*9+Rs*5+Bs*3+Ks*3+Ps*1
             //int almost_promoted = 0, // white pawns on 7th rank - black pawns on 2nd rank
             int king_protection = 0, // count pieces around the king
             //int coverage = 0, // count squares controlled by white - black
-            int legal_moves = 0 // number of legal moves
+            int legal_moves = 0, // number of legal moves
+			int control_center = 0 // number of pieces in the center
         >
         int evaluate()
         {
             int ret = 0;
             if constexpr (material > 0) ret += material * get_material();
             //if const (almost_promoted)ret += almost_promoted * almost_promoted_evaluate();
-            if constexpr (king_protection)ret += king_protection * king_protection_eval();
+            if constexpr (king_protection > 0) ret += king_protection * king_protection_eval();
             //if const (coverage)ret += coverage * coverage_evaluate();
-            if constexpr (legal_moves > 0) ret += legal_moves * count_all_legal_moves() * turn();
+            if constexpr (legal_moves > 0) ret += legal_moves * count_all_legal_moves() * int(turn());
+			if constexpr (control_center > 0) ret += control_center * count_center_pieces();
             return ret;
         }
 
